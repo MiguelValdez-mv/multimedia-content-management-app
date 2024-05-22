2a01970cbbab88d0f392e9c3a64a9b8faa86841c
Miguel Valdez <mavd22mv@gmail.com>
2024-05-22 11:52:36 -0400

    Fixes

--- frontend/src/components/app/thematics/ThematicList/index.jsx
+++ frontend/src/components/app/thematics/ThematicList/index.jsx
@@ -1,12 +1,15 @@
 import { ThematicListItem } from "../ThematicListItem";
 import { Col } from "../../../layout/Col";
+import { NoDataYet } from "../../../molecules/NoDataYet";
 
 export function ThematicList({ thematics }) {
   return (
     <Col className="gap-2">
-      {thematics.map((t) => (
-        <ThematicListItem thematic={t} />
-      ))}
+      {thematics.length ? (
+        thematics.map((t) => <ThematicListItem thematic={t} />)
+      ) : (
+        <NoDataYet />
+      )}
     </Col>
   );
 }
